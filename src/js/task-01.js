53053fe22d708e64e60f71ad47c963c63dfd3476
Serhii Yatsenko <serhii2111@yahoo.com>
2022-10-25 11:17:58 +0300

    allmost all

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,2 +1,11 @@
-const items = document.querySelector('.item');
-console.log(items);
+const navElement = document.querySelector('#categories');
+const listItem = navElement.querySelectorAll('.item');
+
+console.log(`Number of categories: ${listItem.length}`);
+console.log("");
+
+for (let i = 0; i < listItem.length; i++) {
+    console.log(`Category: + listItem[i].querySelector('h2').textContent`);
+    console.log(`Elements: + listItem[i].querySelectorAll('ul li').length`);
+    // console.log("");
+}
---
+++ src/js/task-07.js
@@ -0,0 +1,5 @@
+const inputRange = document.querySelector('#font-size-control');
+
+const spanText = document.querySelector('#text');
+spanText.style.fontSize = `${Number(inputRange.value)}px`;
+console.log(spanText.style);
